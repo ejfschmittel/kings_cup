4e47b77f043ac48f13c9da3e8eee63248ddd9059
ejfschmittel <ejfschmittel@gmail.com>
2023-06-04 16:48:19 +0200

    bugfix: Add terraform deployment action

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ejfschmittel-terraform-state"
-    key            = "apps/s3_starter/terraform.tfstate"
+    key            = "apps/ejf-kings-cup/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
